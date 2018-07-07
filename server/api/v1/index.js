aa08db1365a0b20aaa4ec835078c75553922d873
Arturo Vittorino <arturovito@gmail.com>
2018-07-07 13:27:04 -0500

    Add Comments and Users resources

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const users = require('./users/routes');
 const posts = require('./posts/routes');
+const comments = require('./comments/routes');
 
-router.use('/post', posts);
+router.use('/comments', comments);
+router.use('/posts', posts);
+router.use('/users', users);
 
 module.exports = router;
